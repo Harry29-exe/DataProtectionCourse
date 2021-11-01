1540d29b85d8de2e43b90b2cbab35fc58fdf48a9
Kamil Wójcik <wojcikakamil@gmail.com>
2021-11-01 16:52:58 +0100

    working decrypting security_ECB_encrypted.bmp and sort of security_CBC_encrypted.bmp

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -1,3 +1,4 @@
+import string
 from math import log2
 
 import numpy as np
@@ -46,7 +47,53 @@ def decrypt_full(input_filename, mode, key, iv=None):
 
 
 path = "/home/kamil/PycharmProjects/DataProtectionCourse/lab3/"
-key = get_random_bytes(16)
-BMPcrypt.encrypt_full(path + 'demo24_copy.bmp', "ECB", key)
-print("success")
-decrypt_full(path + "demo24_copy_ECB_encrypted.bmp", "ECB", key)
+filename = "security_ECB_encrypted.bmp"
+
+possibleValue = string.ascii_lowercase + string.digits
+img_data = open(path + filename, "rb").read()
+real_key = None
+first_128_bits = ""
+for i in possibleValue:
+    key = ""
+    for j in range(16):
+        key += i
+    key_bytes = bytes(key, 'ASCII')
+
+    aes = AES.new(key=key_bytes, mode=AES.MODE_ECB)
+    decrypted_data = aes.decrypt(BMPcrypt.nullpadding(img_data))
+    if calc_entropy(calc_prob(decrypted_data[:14])) < 3:
+        print(key)
+        real_key = key_bytes
+        first_128_bits = decrypted_data[:128]
+
+# decrypt_full(path + filename, "ECB", real_key)
+
+
+cbc_filename = "security_CBC_encrypted.bmp"
+def decrypt_full_mod(input_filename, mode, key, first_128):
+    img_in = open(input_filename, "rb")
+    data = img_in.read()
+    if mode == "ECB":
+        aes = AES.new(key=key, mode=AES.MODE_ECB)
+    else:
+        aes = AES.new(key=key, mode=AES.MODE_CBC)
+
+    decrypted_data = aes.decrypt(BMPcrypt.nullpadding(data))
+    decrypted_data = first_128 + decrypted_data[128:]
+
+    in_name_parts = input_filename.split('.')
+    in_name = in_name_parts[0].split('_')
+    # mode = in_name[len(in_name) - 2]
+
+    output_filename = "decrypted.bmp"
+    img_out = open(output_filename, "wb")
+    img_out.write(decrypted_data)
+    img_out.close()
+    img_in.close()
+
+
+decrypt_full_mod(path + cbc_filename, "CBC", real_key, first_128_bits)
+# key = get_random_bytes(16)
+# BMPcrypt.encrypt_full(path + 'demo24_copy.bmp', "ECB", key)
+# print("success")
+# decrypt_full(path + "demo24_copy_ECB_encrypted.bmp", "ECB", key)
